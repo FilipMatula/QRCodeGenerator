87a5e45e299f08a64d13e131cf7673ebb70b5b97
ykp1469 <filip.matula@mtupolska.com>
2021-05-29 08:43:04 +0200

    - less types of codes

--- CodeScannerGenerator/Common/Constants.cs
+++ CodeScannerGenerator/Common/Constants.cs
@@ -9,7 +9,9 @@ namespace CodeScannerGenerator.Common
 {
     public static class Constants
     {
-        public static List<BarcodeFormat> PossibleFormats { get; set; } = new List<BarcodeFormat>() { BarcodeFormat.CODABAR, BarcodeFormat.CODE_128, BarcodeFormat.CODE_39, BarcodeFormat.CODE_93, BarcodeFormat.DATA_MATRIX, BarcodeFormat.QR_CODE, BarcodeFormat.EAN_13, BarcodeFormat.EAN_8};
+        //public static List<BarcodeFormat> PossibleFormats { get; set; } = new List<BarcodeFormat>() { BarcodeFormat.CODABAR, BarcodeFormat.CODE_128, BarcodeFormat.CODE_39, BarcodeFormat.CODE_93, BarcodeFormat.DATA_MATRIX, BarcodeFormat.QR_CODE, BarcodeFormat.EAN_13, BarcodeFormat.EAN_8 };
+        public static List<BarcodeFormat> PossibleFormats { get; set; } = new List<BarcodeFormat>() { BarcodeFormat.QR_CODE, BarcodeFormat.CODE_128, BarcodeFormat.DATA_MATRIX };
+
         //public static List<BarcodeFormat> PossibleFormats { get; set; } = Enum.GetValues(typeof(BarcodeFormat)).Cast<BarcodeFormat>().Where(t => t != BarcodeFormat.PHARMA_CODE).ToList();
     }
 }
