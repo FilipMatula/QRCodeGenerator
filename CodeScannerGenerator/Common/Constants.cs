5b0cbe938829f657918759446f2bb2bcf4575ba1
MATULA, Filip <filip.matula@mtupolska.com>
2023-10-11 08:49:00 +0200

    - added code_39

--- CodeScannerGenerator/Common/Constants.cs
+++ CodeScannerGenerator/Common/Constants.cs
@@ -10,7 +10,7 @@ namespace CodeScannerGenerator.Common
     public static class Constants
     {
         public static List<BarcodeFormat> BarcodeFormats { get; set; } = new List<BarcodeFormat>() { BarcodeFormat.CODE_128, BarcodeFormat.CODE_39, BarcodeFormat.CODE_93, BarcodeFormat.CODABAR, BarcodeFormat.EAN_13, BarcodeFormat.EAN_8 };
-        public static List<BarcodeFormat> PossibleFormats { get; set; } = new List<BarcodeFormat>() { BarcodeFormat.CODE_128, BarcodeFormat.DATA_MATRIX, BarcodeFormat.QR_CODE};
+        public static List<BarcodeFormat> PossibleFormats { get; set; } = new List<BarcodeFormat>() { BarcodeFormat.CODE_128, BarcodeFormat.DATA_MATRIX, BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39};
         //public static List<BarcodeFormat> PossibleFormats { get; set; } = new List<BarcodeFormat>() { BarcodeFormat.QR_CODE, BarcodeFormat.CODE_128, BarcodeFormat.DATA_MATRIX };
 
         //public static List<BarcodeFormat> PossibleFormats { get; set; } = Enum.GetValues(typeof(BarcodeFormat)).Cast<BarcodeFormat>().Where(t => t != BarcodeFormat.PHARMA_CODE).ToList();
